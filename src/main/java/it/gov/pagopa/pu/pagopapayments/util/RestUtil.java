15f6ec3a2609ed34f6c00bc30b197a284b98d6bd
serdimic <sergio.dimichele@eng.it>
2025-01-09 10:04:22 +0100

    P4ADEV-1674 add specific handling for NotFound on RestUtil

--- src/main/java/it/gov/pagopa/pu/pagopapayments/util/RestUtil.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/util/RestUtil.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.pu.pagopapayments.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClientException;
 
@@ -15,6 +16,11 @@ public class RestUtil {
       httpCallStart = System.currentTimeMillis();
     try{
       return invoker.get();
+    } catch (HttpClientErrorException.NotFound nfe) {
+      int statusCode = nfe.getStatusCode().value();
+      String body = nfe.getResponseBodyAsString();
+      log.warn("HttpClientErrorException.NotFound on {} - returned code[{}] body[{}]", logErrorDetail.get(), statusCode, body);
+      return null;
     } catch (HttpServerErrorException he) {
       int statusCode = he.getStatusCode().value();
       String body = he.getResponseBodyAsString();

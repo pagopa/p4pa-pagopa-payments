5f7710286935fe61acddde656b4f96eb06b3ef19
serdimic <sergio.dimichele@eng.it>
2025-01-23 12:45:15 +0100

    P4ADEV-1776 improve soap client implementation and set http header for api-key

--- src/main/java/it/gov/pagopa/pu/pagopapayments/connector/soap/NodeForPaClientImpl.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/connector/soap/NodeForPaClientImpl.java
@@ -2,16 +2,31 @@ package it.gov.pagopa.pu.pagopapayments.connector.soap;
 
 import gov.telematici.pagamenti.ws.NodoChiediElencoFlussiRendicontazione;
 import gov.telematici.pagamenti.ws.NodoChiediElencoFlussiRendicontazioneRisposta;
+import org.springframework.ws.client.core.WebServiceMessageCallback;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.SoapMessage;
+import org.springframework.ws.transport.context.TransportContext;
+import org.springframework.ws.transport.context.TransportContextHolder;
+import org.springframework.ws.transport.http.HttpUrlConnection;
 
-public class NodeForPaClientImpl extends AbstractBaseClientImpl implements NodeForPaClient {
+public class NodeForPaClientImpl extends WebServiceGatewaySupport implements NodeForPaClient {
 
-  public final static String SUBSCRIPTION_KEY_KEY = "Ocp-Apim-Subscription-Key";
+  public static final String SUBSCRIPTION_KEY_KEY = "Ocp-Apim-Subscription-Key";
 
   @Override
   public NodoChiediElencoFlussiRendicontazioneRisposta nodoChiediElencoFlussiRendicontazione(NodoChiediElencoFlussiRendicontazione request, String apiKey) {
-    //TODO XXX.setHeader(SUBSCRIPTION_KEY_KEY, apiKey);
-    NodoChiediElencoFlussiRendicontazioneRisposta response = (NodoChiediElencoFlussiRendicontazioneRisposta)
-      getWebServiceTemplate().marshalSendAndReceive(request, getMessageCallback());
-    return response;
+    return (NodoChiediElencoFlussiRendicontazioneRisposta)
+      getWebServiceTemplate().marshalSendAndReceive(request, getMessageCallback(apiKey, "nodoChiediElencoFlussiRendicontazione"));
+  }
+
+  private WebServiceMessageCallback getMessageCallback(String apiKey, String soapAction) {
+    return message -> {
+      //fix SoapAction header not included automatically by Spring WS
+      ((SoapMessage)message).setSoapAction(soapAction);
+      //set subscription key
+      TransportContext context = TransportContextHolder.getTransportContext();
+      HttpUrlConnection connection = (HttpUrlConnection) context.getConnection();
+      connection.addRequestHeader(SUBSCRIPTION_KEY_KEY, apiKey);
+    };
   }
 }

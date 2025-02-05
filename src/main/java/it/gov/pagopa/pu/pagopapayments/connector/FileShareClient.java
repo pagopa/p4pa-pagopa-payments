f82a5977fbd94de9a41cad65943055adffaa0811
domenicogi <domenico.giacobbe@eng.it>
2025-02-05 17:50:30 +0100

    Update src/main/java/it/gov/pagopa/pu/pagopapayments/connector/FileShareClient.java
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/pu/pagopapayments/connector/FileShareClient.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/connector/FileShareClient.java
@@ -7,5 +7,5 @@ import it.gov.pagopa.pu.pagopapayments.dto.PaSendRtDTO;
 public interface FileShareClient {
 
   String uploadRt(PaSendRtDTO paSendRtDTO, Organization organization, String accessToken);
-  String fetchPaymentReporting(PaPaymentReportingDTO paPaymentReporingDTO, Long organizationId, String accessToken);
+  String uploadPaymentReporting(PaPaymentReportingDTO paPaymentReporingDTO, Long organizationId, String accessToken);
 }

bcd3ad0556d7707604daf828eadff743c48cb77f
antonio.torre <antonio.torre@pagopa.it>
2025-02-07 11:53:27 +0100

    fix: fix upload-file-share return type

--- src/test/java/it/gov/pagopa/pu/pagopapayments/exception/PagoPaPaymentsExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/pu/pagopapayments/exception/PagoPaPaymentsExceptionHandlerTest.java
@@ -148,6 +148,14 @@ class PagoPaPaymentsExceptionHandlerTest {
   }
 
   @Test
+  void handleUrlNotFound() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/NOTEXISTENTURL"))
+      .andExpect(MockMvcResultMatchers.status().isNotFound())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("PAGOPA_PAYMENTS_NOT_FOUND"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No static resource NOTEXISTENTURL."));
+  }
+
+  @Test
   void handleNoBodyException() throws Exception {
     performRequest(DATA, MediaType.APPLICATION_JSON, null)
       .andExpect(MockMvcResultMatchers.status().isBadRequest())

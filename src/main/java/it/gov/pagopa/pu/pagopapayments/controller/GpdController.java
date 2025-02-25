9f3c631135d79f88717d78477d44b1c33efbe211
dgiacobbe <domenico.giacobbe@eng.it>
2025-02-25 09:02:37 +0100

    P4PADEV-2118 controller mapper and service for api sync gpd

---
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/controller/GpdController.java
@@ -0,0 +1,27 @@
+package it.gov.pagopa.pu.pagopapayments.controller;
+
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.pagopapayments.controller.generated.GpdApi;
+import it.gov.pagopa.pu.pagopapayments.service.gpd.GpdFacadeService;
+import it.gov.pagopa.pu.pagopapayments.util.SecurityUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class GpdController implements GpdApi {
+
+  private final GpdFacadeService gpdFacadeService;
+
+  public GpdController(GpdFacadeService gpdFacadeService){
+    this.gpdFacadeService = gpdFacadeService;
+  }
+
+  @Override
+  public ResponseEntity<Void> syncGpd(String iud, DebtPositionDTO debtPositionDTO) {
+    log.info("invoking syncAca, iud[{}] debtPositionDTO[{}]", iud, debtPositionDTO.getDebtPositionId());
+    gpdFacadeService.sync(iud, debtPositionDTO, SecurityUtils.getAccessToken());
+    return ResponseEntity.ok().build();
+  }
+}
---
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/service/gpd/GpdFacadeService.java
@@ -0,0 +1,63 @@
+package it.gov.pagopa.pu.pagopapayments.service.gpd;
+
+import it.gov.pagopa.nodo.gpd.dto.generated.PaymentPositionModel;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.organization.dto.generated.BrokerApiKeys;
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
+import it.gov.pagopa.pu.pagopapayments.connector.pagopa.gpd.GpdService;
+import it.gov.pagopa.pu.pagopapayments.dto.BrokerForNodoPaDTO;
+import it.gov.pagopa.pu.pagopapayments.mapper.GpdDebtPositionMapper;
+import it.gov.pagopa.pu.pagopapayments.service.broker.BrokerRetrieverService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class GpdFacadeService {
+
+  private final GpdService gpdService;
+  private final GpdDebtPositionMapper gpdDebtPositionMapper;
+  private final BrokerRetrieverService brokerRetrieverService;
+
+  public GpdFacadeService(
+    GpdService gpdService,
+    GpdDebtPositionMapper gpdDebtPositionMapper,
+    BrokerRetrieverService brokerRetrieverService
+  ) {
+    this.gpdService = gpdService;
+    this.gpdDebtPositionMapper = gpdDebtPositionMapper;
+    this.brokerRetrieverService = brokerRetrieverService;
+  }
+
+  public void sync(String iud, DebtPositionDTO debtPosition, String accessToken) {
+    invokeCreatePositionImpl(iud, debtPosition, accessToken);
+  }
+
+  private void invokeCreatePositionImpl(String iud, DebtPositionDTO debtPositionDTO, String accessToken) {
+    Pair<BrokerApiKeys, String> brokerData = brokerRetrieverService.getBrokerApiKeyAndSegregationCodesByOrganizationId(debtPositionDTO.getOrganizationId(), accessToken);
+    BrokerForNodoPaDTO brokerForNodoPaDTO = brokerRetrieverService.getBrokerForNodoPaDTOByOrganizationId(debtPositionDTO.getOrganizationId(), accessToken);
+    Organization organization = brokerForNodoPaDTO.getOrganization();
+    Pair<GpdDebtPositionMapper.OPERATION, PaymentPositionModel> debtPostionToSendGPD = gpdDebtPositionMapper.mapToNewPaymentPositionModel(iud, debtPositionDTO,organization);
+
+
+    PaymentPositionModel newPaymentPositionModel = debtPostionToSendGPD.getRight();
+    GpdDebtPositionMapper.OPERATION operation = debtPostionToSendGPD.getLeft();
+    if (operation == GpdDebtPositionMapper.OPERATION.DELETE) {
+      log.info("invoking GPD paDeletePosition for installment[{}/{}]",
+        newPaymentPositionModel.getFiscalCode(), newPaymentPositionModel.getPaymentOption().getFirst().getIuv());
+      gpdService.paDeletePosition(brokerData.getLeft().getGpdKey(), organization.getOrgFiscalCode(), newPaymentPositionModel.getIupd());
+    }
+    if (operation == GpdDebtPositionMapper.OPERATION.CREATE) {
+      log.info("invoking GPD paCreatePosition for installment[{}/{}]",
+        newPaymentPositionModel.getFiscalCode(), newPaymentPositionModel.getPaymentOption().getFirst().getIuv());
+      gpdService.paCreatePosition(brokerData.getLeft().getGpdKey(), organization.getOrgFiscalCode(), newPaymentPositionModel, true);
+    }
+    if (operation == GpdDebtPositionMapper.OPERATION.UPDATE) {
+      log.info("invoking GPD paUpdatePosition for installment[{}/{}]",
+        newPaymentPositionModel.getFiscalCode(), newPaymentPositionModel.getPaymentOption().getFirst().getIuv());
+      gpdService.paUpdatePosition(brokerData.getLeft().getGpdKey(), organization.getOrgFiscalCode(), newPaymentPositionModel.getIupd(),newPaymentPositionModel, true);
+    }
+  }
+
+}

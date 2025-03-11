a07407e843a1ca466d400d623235782621807794
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-03-11 16:24:02 +0100

    fix gpd date conversion

--- src/main/java/it/gov/pagopa/pu/pagopapayments/mapper/GpdDebtPositionMapper.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/mapper/GpdDebtPositionMapper.java
@@ -50,7 +50,7 @@ public class GpdDebtPositionMapper {
           .switchToExpired(installment.getDueDate() != null)
           .companyName(org.getOrgName())
           .paymentOption(List.of(getPaymentOption(installment)))
-          .validityDate(ConversionUtils.atEndOfDay(debtPosition.getValidityDate()))
+          .validityDate(ConversionUtils.atEndOfDay(debtPosition.getValidityDate()).toString())
         );
       }).findAny().orElseThrow(() -> new InvalidValueException("Installment with IUD[%s] on debtPosition[%s] not found or with invalid sync state".formatted(iud, debtPosition.getDebtPositionId())));
   }
@@ -86,7 +86,7 @@ public class GpdDebtPositionMapper {
       .amount(installment.getAmountCents())
       .description(installment.getRemittanceInformation())
       .isPartialPayment(false)
-      .dueDate(ConversionUtils.atEndOfDay(installment.getDueDate()))
+      .dueDate(ConversionUtils.atEndOfDay(installment.getDueDate()).toString())
       .fee(0L)
       .notificationFee(0L)
       .paymentOptionMetadata(List.of(PaymentOptionMetadataModel.builder()

50d9ab49ab8725503b185358f79dd89bc1f24bb6
antoniocalo <antonio.calo@nttdata.com>
2025-05-05 10:26:03 +0200

    P4ADEV-2764 add send client

---
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/connector/send_notification/SendNotificationServiceImpl.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.pu.pagopapayments.connector.send_notification;
+
+import it.gov.pagopa.pu.pagopapayments.connector.send_notification.client.SendClient;
+import it.gov.pagopa.pu.sendnotification.dto.generated.NotificationPriceResponseV23DTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendNotificationServiceImpl implements SendNotificationService{
+  private final SendClient sendClient;
+
+  public SendNotificationServiceImpl(SendClient sendClient) {
+    this.sendClient = sendClient;
+  }
+
+  @Override
+  public NotificationPriceResponseV23DTO retrieveNotificationPrice(Long organizationId, String nav, String accessToken) {
+    return sendClient.retrieveNotificationPrice(organizationId, nav, accessToken);
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/pagopapayments/connector/send_notification/config/SendNotificationApisHolderTest.java
@@ -0,0 +1,50 @@
+package it.gov.pagopa.pu.pagopapayments.connector.send_notification.config;
+
+import it.gov.pagopa.pu.pagopapayments.connector.BaseApiHolderTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@ExtendWith(MockitoExtension.class)
+class SendNotificationApisHolderTest extends BaseApiHolderTest {
+
+  @Mock
+  private RestTemplateBuilder restTemplateBuilderMock;
+
+  private SendNotificationApisHolder sendNotificationApisHolder;
+
+  @BeforeEach
+  void setUp() {
+    Mockito.when(restTemplateBuilderMock.build()).thenReturn(restTemplateMock);
+    Mockito.when(restTemplateMock.getUriTemplateHandler()).thenReturn(new DefaultUriBuilderFactory());
+    SendNotificationApiClientConfig clientConfig = SendNotificationApiClientConfig.builder()
+      .baseUrl("http://example.com")
+      .build();
+    sendNotificationApisHolder = new SendNotificationApisHolder(clientConfig, restTemplateBuilderMock);
+  }
+
+  @AfterEach
+  void verifyNoMoreInteractions() {
+    Mockito.verifyNoMoreInteractions(
+      restTemplateBuilderMock,
+      restTemplateMock
+    );
+  }
+
+  @Test
+  void whenGetSendApiThenAuthenticationShouldBeSetInThreadSafeMode() throws InterruptedException {
+    assertAuthenticationShouldBeSetInThreadSafeMode(
+      accessToken -> sendNotificationApisHolder.getSendApi(accessToken)
+        .retrieveNotificationPrice(1L, "NAV"),
+      new ParameterizedTypeReference<>() {},
+      sendNotificationApisHolder::unload
+    );
+  }
+}

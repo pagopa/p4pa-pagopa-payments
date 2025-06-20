aa80069e45f3eb741b809e9744ab33cbdf65b8c3
antonio.torre <antonio.torre@pagopa.it>
2025-06-20 18:48:50 +0200

    fix cycle dependencies

--- src/main/java/it/gov/pagopa/pu/pagopapayments/util/Utilities.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/util/Utilities.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.pu.pagopapayments.util;
 
-import it.gov.pagopa.pu.pagopapayments.registry.RegistryLogger;
 import org.slf4j.MDC;
 
 import java.util.Arrays;
@@ -10,6 +9,8 @@ public class Utilities {
   private Utilities() {
   }
 
+  public static final String IUV_SEPARATOR = ",";
+
   public static String getTraceId() {
     return MDC.get("traceId");
   }
@@ -17,10 +18,10 @@ public class Utilities {
   public static String iuv2Nav(String iuv) {
     if (iuv == null || iuv.isBlank()) {
       return null;
-    } else if (iuv.contains(RegistryLogger.IUV_SEPARATOR)) {
-      return Arrays.stream(iuv.split(RegistryLogger.IUV_SEPARATOR))
+    } else if (iuv.contains(IUV_SEPARATOR)) {
+      return Arrays.stream(iuv.split(IUV_SEPARATOR))
         .map(Utilities::iuv2Nav)
-        .collect(Collectors.joining(RegistryLogger.IUV_SEPARATOR));
+        .collect(Collectors.joining(IUV_SEPARATOR));
     } else {
       return Constants.AUX_DIGIT + iuv;
     }
@@ -29,10 +30,10 @@ public class Utilities {
   public static String nav2Iuv(String nav) {
     if (nav == null || nav.isBlank()) {
       return null;
-    } else if (nav.contains(RegistryLogger.IUV_SEPARATOR)) {
-      return Arrays.stream(nav.split(RegistryLogger.IUV_SEPARATOR))
+    } else if (nav.contains(IUV_SEPARATOR)) {
+      return Arrays.stream(nav.split(IUV_SEPARATOR))
               .map(Utilities::nav2Iuv)
-              .collect(Collectors.joining(RegistryLogger.IUV_SEPARATOR));
+              .collect(Collectors.joining(IUV_SEPARATOR));
     } else if (nav.length() < 2 || !nav.startsWith(Constants.AUX_DIGIT)) {
       throw new IllegalArgumentException("Invalid NAV format: " + nav);
     } else {

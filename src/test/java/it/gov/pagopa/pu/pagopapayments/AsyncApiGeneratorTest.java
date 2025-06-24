a644003275bdfd1bd83b8864d9006cc4327ce36d
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2025-06-24 10:59:32 +0200

    feat: P4ADEV-3087 asyncApi generation (#105)

--- src/test/java/it/gov/pagopa/pu/pagopapayments/AsyncApiGeneratorTest.java
+++ src/test/java/it/gov/pagopa/pu/pagopapayments/AsyncApiGeneratorTest.java
@@ -1,10 +1,7 @@
 package it.gov.pagopa.pu.pagopapayments;
 
-import io.github.springwolf.core.asyncapi.schemas.converters.SchemaTitleModelConverter;
-import io.swagger.v3.core.converter.ModelConverters;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -25,60 +22,48 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE, addFilters = false)
 @TestPropertySource(properties = {
-  "logging.level.org.springdoc.core.utils.SpringDocAnnotationsUtils=OFF",
-  "springdoc.api-docs.enabled=true",
-  "springdoc.swagger-ui.enabled=false",
-  "springwolf.enabled=false",
+  "springwolf.enabled=true",
+  "springwolf.use-fqn=false",
 })
 @Slf4j
-class OpenApiGeneratorTest {
+class AsyncApiGeneratorTest {
 
   @Autowired
   private MockMvc mockMvc;
 
-  @BeforeEach
-  void init() {
-    // removing ModelConverters configured by SpringWolf which will cause the setting of the title in each schema
-    boolean openapi31 = true;
-    ModelConverters modelConverters = ModelConverters.getInstance(openapi31);
-    modelConverters.getConverters().stream()
-      .filter(SchemaTitleModelConverter.class::isInstance)
-      .forEach(modelConverters::removeConverter);
-  }
-
   @Test
   void generateAndVerifyCommit() throws Exception {
     MvcResult result = mockMvc.perform(
-      get("/v3/api-docs")
+      get("/springwolf/docs")
         .contentType(MediaType.APPLICATION_JSON)
         .accept(MediaType.APPLICATION_JSON)
     ).andExpect(status().isOk())
       .andReturn();
 
-    String openApiResult = result.getResponse().getContentAsString()
+    String asyncApiResult = result.getResponse().getContentAsString()
       .replace("\r", "");
 
-    Assertions.assertTrue(openApiResult.startsWith("{\n  \"openapi\" : \"3."));
+    Assertions.assertTrue(asyncApiResult.startsWith("{\n  \"asyncapi\": \"3"));
 
-    Path openApiGeneratedPath = Path.of("openapi/generated.openapi.json");
+    Path asyncApiGeneratedPath = Path.of("asyncapi/generated.asyncapi.json");
     boolean toStore=true;
-    if(Files.exists(openApiGeneratedPath)){
-      String storedOpenApi = Files.readString(openApiGeneratedPath);
+    if(Files.exists(asyncApiGeneratedPath)){
+      String storedOpenApi = Files.readString(asyncApiGeneratedPath);
       try {
-        JsonAssert.comparator(JsonCompareMode.STRICT).assertIsMatch(storedOpenApi, openApiResult);
+        JsonAssert.comparator(JsonCompareMode.STRICT).assertIsMatch(storedOpenApi, asyncApiResult);
         toStore=false;
       } catch (Throwable e){
         log.info("Observed the following changes: {}", e.getMessage());
       }
     }
     if(toStore){
-      Files.writeString(openApiGeneratedPath, openApiResult, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+      Files.writeString(asyncApiGeneratedPath, asyncApiResult, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 
     String gitStatus = execCmd("git", "status");
-    Assertions.assertFalse(gitStatus.contains("openapi/generated.openapi.json"), "Generated OpenApi not committed");
+    Assertions.assertFalse(gitStatus.contains("asyncapi/generated.asyncapi.json"), "Generated AsyncApi not committed");
   }
 
   public static String execCmd(String... cmd) throws java.io.IOException {

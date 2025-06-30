7d9175cdb26a7acf79f9e9efc311ab7eeeb62048
antonio.torre <antonio.torre@pagopa.it>
2025-06-30 13:24:28 +0200

    P4ADEV-3166 Publishing paGetPayment to registry

--- src/main/java/it/gov/pagopa/pu/pagopapayments/endpoint/PaForNodeEndpoint.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/endpoint/PaForNodeEndpoint.java
@@ -100,23 +100,35 @@ public class PaForNodeEndpoint {
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "paGetPaymentV2Request")
   @ResponsePayload
   public PaGetPaymentV2Response paGetPaymentV2(@RequestPayload PaGetPaymentV2Request request) {
-    long startTime = System.currentTimeMillis();
-    try {
-      log.info("processing paGetPaymentV2 idPA[{}] notice[{}/{}]", request.getIdPA(), request.getQrCode().getFiscalCode(), request.getQrCode().getNoticeNumber());
-      RetrievePaymentDTO retrievePaymentDTO = PaGetPaymentMapper.paPaGetPaymentV2Request2RetrievePaymentDTO(request);
-      Pair<InstallmentDTO, Organization> installmentAndOrganization = synchronousPaymentService.retrievePayment(retrievePaymentDTO);
-      return PaGetPaymentMapper.installmentDto2PaGetPaymentV2Response(
-        installmentAndOrganization.getLeft(), installmentAndOrganization.getRight(), request.getTransferType());
-    } catch (PagoPaNodeFaultException spe) {
-      log.error("Fault in paGetPaymentV2 [{}/{}] {}", request.getQrCode().getFiscalCode(), request.getQrCode().getNoticeNumber(), spe.getErrorCode());
-      return handleFault(spe.getErrorCode(), spe.getErrorEmitter(), new PaGetPaymentV2Response());
-    } catch (Exception e) {
-      log.error("Error in paGetPaymentV2 [{}/{}]", request.getQrCode().getFiscalCode(), request.getQrCode().getNoticeNumber(), e);
-      return handleFault(PagoPaNodeFaults.PAA_SYSTEM_ERROR, request.getIdPA(), new PaGetPaymentV2Response());
-    } finally {
-      long elapsed = System.currentTimeMillis() - startTime;
-      log.info("SOAP WS paGetPaymentV2, elapsed time[{}]", elapsed);
-    }
+    RegistryContextData contextData = RegistryContextData.builder()
+      .orgFiscalCode(request.getIdPA())
+      .brokerStationId(request.getIdStation())
+      .eventType(RegistryEventType.paGetPaymentV2)
+      .iuv(Utilities.nav2Iuv(request.getQrCode().getNoticeNumber()))
+      .build();
+
+    return registryLogger.execute(
+      contextData,
+      request,
+      () -> {
+        log.info("processing paGetPaymentV2 idPA[{}] notice[{}/{}]", request.getIdPA(), request.getQrCode().getFiscalCode(), request.getQrCode().getNoticeNumber());
+        RetrievePaymentDTO retrievePaymentDTO = PaGetPaymentMapper.paPaGetPaymentV2Request2RetrievePaymentDTO(request);
+        Pair<InstallmentDTO, Organization> installmentAndOrganization = synchronousPaymentService.retrievePayment(retrievePaymentDTO);
+        return Triple.of(
+          PaGetPaymentMapper.installmentDto2PaGetPaymentV2Response(
+            installmentAndOrganization.getLeft(), installmentAndOrganization.getRight(), request.getTransferType()),
+          null,
+          RegistryOutcome.OK);
+      },
+      e -> {
+        if (e instanceof PagoPaNodeFaultException spe) {
+          log.error("Fault in paGetPaymentV2 [{}/{}] {}", request.getQrCode().getFiscalCode(), request.getQrCode().getNoticeNumber(), spe.getErrorCode());
+          return handleFault(spe.getErrorCode(), spe.getErrorEmitter(), new PaGetPaymentV2Response());
+        } else {
+          log.error("Error in paGetPaymentV2 [{}/{}]", request.getQrCode().getFiscalCode(), request.getQrCode().getNoticeNumber(), e);
+          return handleFault(PagoPaNodeFaults.PAA_SYSTEM_ERROR, request.getIdPA(), new PaGetPaymentV2Response());
+        }
+      });
   }
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "paSendRTReq")

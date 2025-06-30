87dc8524825513e06333dd8a651ea5447e49f3aa
antoniocalo <antonio.calo@nttdata.com>
2025-06-30 16:07:24 +0200

    P4ADEV-3311 add pusil client

--- src/main/java/it/gov/pagopa/pu/pagopapayments/connector/pu_sil/client/PuSilClient.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/connector/pu_sil/client/PuSilClient.java
@@ -4,7 +4,6 @@ import it.gov.pagopa.pu.pagopapayments.connector.pu_sil.config.PuSilApisHolder;
 import it.gov.pagopa.pu.pusil.dto.generated.AmountUpdatesDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 @Slf4j
@@ -17,11 +16,6 @@ public class PuSilClient {
   }
 
   public AmountUpdatesDTO getAmountUpdates(Long orgSilServiceId, String nav, String accessToken) {
-    try {
       return apisHolder.getAmountUpdatesApi(accessToken).getAmountUpdates(orgSilServiceId, nav);
-    } catch (HttpClientErrorException.NotFound e){
-      log.info("Cannot find amount having orgSilServiceId {} and nav {}", orgSilServiceId, nav);
-      return null;
-    }
   }
 }

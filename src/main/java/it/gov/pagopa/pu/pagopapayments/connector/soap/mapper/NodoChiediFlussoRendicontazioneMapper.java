e96565e07bc4b941eb2cfec846f2a50cac0d4054
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-07-01 19:02:56 +0200

    feat: P4ADEV-3164 Publishing fetchPaymentReporting to registry (#113)

---
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/connector/soap/mapper/NodoChiediFlussoRendicontazioneMapper.java
@@ -0,0 +1,25 @@
+package it.gov.pagopa.pu.pagopapayments.connector.soap.mapper;
+
+import gov.telematici.pagamenti.ws.NodoChiediFlussoRendicontazione;
+import it.gov.pagopa.pu.organization.dto.generated.Broker;
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
+import it.gov.pagopa.pu.pagopapayments.dto.BrokerForNodoPaDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NodoChiediFlussoRendicontazioneMapper {
+
+  public NodoChiediFlussoRendicontazione createFlussoRendicontazioneRequest(BrokerForNodoPaDTO brokerForNodoPaDTO, String reportingId) {
+    Broker broker = brokerForNodoPaDTO.getBroker();
+    Organization organization = brokerForNodoPaDTO.getOrganization();
+
+    NodoChiediFlussoRendicontazione request = new NodoChiediFlussoRendicontazione();
+    request.setIdentificativoDominio(organization.getOrgFiscalCode());
+    request.setPassword("password");
+    request.setIdentificativoIntermediarioPA(broker.getBrokerFiscalCode());
+    request.setIdentificativoStazioneIntermediarioPA(broker.getStationId());
+    request.setIdentificativoPSP(null);
+    request.setIdentificativoFlusso(reportingId);
+    return request;
+  }
+}

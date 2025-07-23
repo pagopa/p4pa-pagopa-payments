99bd9f19eac3ecd62220fd835a52fffef9cb4189
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2025-07-23 15:04:56 +0200

    fix: P4ADEV-3335 fix logic due date and switch to expired (#123)

--- src/main/java/it/gov/pagopa/pu/pagopapayments/mapper/AcaDebtPositionMapper.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/mapper/AcaDebtPositionMapper.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Component
@@ -56,7 +57,7 @@ public class AcaDebtPositionMapper {
           .description(installment.getRemittanceInformation())
           .amount(installment.getAmountCents().intValue())
           .expirationDate(ConversionUtils.localDate2RomeMaxTime(installment.getDueDate()))
-          .switchToExpired(installment.getDueDate()!=null)
+          .switchToExpired(Optional.ofNullable(installment.getSwitchToExpired()).orElse(false))
           .payStandIn(true));
       }).findAny().orElseThrow(() -> new InvalidValueException("Installment with IUD[%s] on debtPosition[%s] not found or with invalid sync state".formatted(iud, debtPosition.getDebtPositionId())));
   }

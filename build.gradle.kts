18775bddd6002c1972d9caf135d1ecbd765ec6f6
antonio.torre <antonio.torre@pagopa.it>
2025-12-18 12:50:53 +0100

    fix: Fix CVE, upgrade SpringBoot 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
   implementation("org.springframework.boot:spring-boot-starter-restclient")
   implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("org.springframework.data:spring-data-commons")
   implementation("org.springframework.boot:spring-boot-starter-security-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-cache")

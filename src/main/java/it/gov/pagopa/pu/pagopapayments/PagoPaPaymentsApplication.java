31158044816db51f9afe4a5d68fb41f2c292600c
antonio.torre <antonio.torre@pagopa.it>
2025-12-19 16:36:49 +0100

    fix: Setting default TimeZone on main

--- src/main/java/it/gov/pagopa/pu/pagopapayments/PagoPaPaymentsApplication.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/PagoPaPaymentsApplication.java
@@ -1,13 +1,17 @@
 package it.gov.pagopa.pu.pagopapayments;
 
+import it.gov.pagopa.pu.pagopapayments.util.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
+
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class PagoPaPaymentsApplication {
 
 	public static void main(String[] args) {
+    TimeZone.setDefault(Constants.DEFAULT_TIMEZONE);
 		SpringApplication.run(PagoPaPaymentsApplication.class, args);
 	}
 
--- src/main/java/it/gov/pagopa/pu/pagopapayments/util/Constants.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/util/Constants.java
@@ -1,12 +1,14 @@
 package it.gov.pagopa.pu.pagopapayments.util;
 
 import java.time.ZoneId;
+import java.util.TimeZone;
 
 public class Constants {
 
   private Constants(){}
 
   public static final ZoneId ZONEID = ZoneId.of("Europe/Rome");
+  public static final TimeZone DEFAULT_TIMEZONE = TimeZone.getTimeZone(ZONEID);
 
   public static final String AUX_DIGIT = "3";
 

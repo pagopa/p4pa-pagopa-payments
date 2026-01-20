421a91cb737fa0aa8a5de2250368196a9d20b862
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-01-20 14:18:16 +0100

    P4ADEV-4297: removed unused import

--- src/main/java/it/gov/pagopa/pu/pagopapayments/service/paymentsreporting/PaymentsReportingSoapServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/service/paymentsreporting/PaymentsReportingSoapServiceImpl.java
@@ -11,7 +11,6 @@ import it.gov.pagopa.pu.pagopapayments.service.broker.BrokerRetrieverService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 
 @Slf4j

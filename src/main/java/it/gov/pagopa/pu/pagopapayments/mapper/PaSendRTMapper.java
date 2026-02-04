117351668aaba1b2c6d494d177c390efc8deb621
michele.mazzini <michele.mazzini1996@gmail.com>
2026-02-04 12:16:04 +0100

    fix: revert

--- src/main/java/it/gov/pagopa/pu/pagopapayments/mapper/PaSendRTMapper.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/mapper/PaSendRTMapper.java
@@ -1,15 +1,11 @@
 package it.gov.pagopa.pu.pagopapayments.mapper;
 
-import it.gov.pagopa.pagopa_api.pa.pafornode.CtReceiptV2;
 import it.gov.pagopa.pagopa_api.pa.pafornode.CtTransferListPAReceiptV2;
 import it.gov.pagopa.pagopa_api.pa.pafornode.CtTransferPAReceiptV2;
 import it.gov.pagopa.pagopa_api.pa.pafornode.PaSendRTV2Request;
 import it.gov.pagopa.pagopa_api.xsd.common_types.v1_0.CtMapEntry;
 import it.gov.pagopa.pagopa_api.xsd.common_types.v1_0.CtMetadata;
 import it.gov.pagopa.pu.debtpositions.dto.generated.ReceiptTransferDTO;
-import it.gov.pagopa.pu.organization.dto.generated.Organization;
-import it.gov.pagopa.pu.pagopapayments.connector.auth.AuthnService;
-import it.gov.pagopa.pu.pagopapayments.connector.organization.OrganizationService;
 import it.gov.pagopa.pu.pagopapayments.dto.PaSendRtDTO;
 import it.gov.pagopa.pu.pagopapayments.service.JAXBTransformService;
 import it.gov.pagopa.pu.pagopapayments.util.Utilities;
@@ -20,39 +16,19 @@ import org.springframework.stereotype.Component;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Component
 @Slf4j
 public class PaSendRTMapper {
 
-  private final OrganizationService organizationService;
-  private final AuthnService authnService;
   private final JAXBTransformService jaxbTransformService;
 
-  public PaSendRTMapper(OrganizationService organizationService, AuthnService authnService, JAXBTransformService jaxbTransformService) {
-    this.organizationService = organizationService;
-    this.authnService = authnService;
+  public PaSendRTMapper(JAXBTransformService jaxbTransformService) {
     this.jaxbTransformService = jaxbTransformService;
   }
 
   public PaSendRtDTO paSendRtV2Request2PaSendRtDTO(PaSendRTV2Request request) {
-    String accessToken = authnService.getAccessToken();
-    Organization organization = organizationService.getOrganizationByFiscalCode(request.getIdPA(), accessToken);
-
-    if(organization == null) {
-      boolean hasValidTransferOrg = request.getReceipt().getTransferList().getTransfers().stream()
-        .map(transfer -> organizationService.getOrganizationByFiscalCode(transfer.getFiscalCodePA(), accessToken))
-        .anyMatch(Objects::nonNull);
-
-      if(hasValidTransferOrg) {
-        organization = organizationService.getOrganizationById(-1L, accessToken);
-        CtReceiptV2 receipt = request.getReceipt();
-        receipt.setFiscalCode(organization.getOrgFiscalCode() + "_" + receipt.getFiscalCode());
-        request.setReceipt(receipt);
-      }
-    }
 
     byte[] receiptBytes = jaxbTransformService.marshallingAsBytes(request, PaSendRTV2Request.class);
 

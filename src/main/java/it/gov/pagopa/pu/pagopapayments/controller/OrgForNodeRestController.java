16271e5a2e5f73b14c8941df3bce430f1192ec3f
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2026-02-17 15:09:50 +0100

    fix: P4ADEV-4328 modify basepath of OrgForNode API (#172)

--- src/main/java/it/gov/pagopa/pu/pagopapayments/controller/OrgForNodeRestController.java
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/controller/OrgForNodeRestController.java
@@ -5,15 +5,17 @@ import it.gov.pagopa.pu.orgfornode.dto.generated.PaymentOptionsResponseForNode;
 import it.gov.pagopa.pu.pagopapayments.service.orgfornode.PaymentOptionsForNodeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/org-for-node/service/v1")
 @Slf4j
-public class PaymentOptionsForNodeController implements PaymentOptionsApi {
+public class OrgForNodeRestController implements PaymentOptionsApi {
 
   private final PaymentOptionsForNodeService service;
 
-  public PaymentOptionsForNodeController(PaymentOptionsForNodeService service) {
+  public OrgForNodeRestController(PaymentOptionsForNodeService service) {
     this.service = service;
   }
 

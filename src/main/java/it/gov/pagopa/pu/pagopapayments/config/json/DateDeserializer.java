974430a971ac46d55246779c76cd76278816b7ba
mscarsel <190512124+MScarsella@users.noreply.github.com>
2026-03-17 15:53:05 +0100

    [P4ADEV-4517] Adds custom date deserializer for PaTaxonomyDTO

---
+++ src/main/java/it/gov/pagopa/pu/pagopapayments/config/json/DateDeserializer.java
@@ -0,0 +1,31 @@
+package it.gov.pagopa.pu.pagopapayments.config.json;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import io.micrometer.common.util.StringUtils;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateDeserializer extends JsonDeserializer<Date> {
+
+  private final SimpleDateFormat italianDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+  @Override
+  public Date deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
+    String dateStr = p.getValueAsString();
+
+    if (StringUtils.isBlank(dateStr) || dateStr.equals("-")) {
+      return null;
+    }
+
+    try {
+      return italianDateFormat.parse(dateStr);
+    } catch (ParseException e) {
+      throw new IOException("Unknown date format: " + dateStr);
+    }
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/pagopapayments/config/json/DateDeserializerTest.java
@@ -0,0 +1,82 @@
+package it.gov.pagopa.pu.pagopapayments.config.json;
+
+import com.fasterxml.jackson.core.JsonParser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+class DateDeserializerTest {
+
+  private final DateDeserializer deserializer = new DateDeserializer();
+
+  @Test
+  void givenValidDateWhenDeserializeThenOk() throws IOException {
+    // Given
+    Date expectedResult = Date.from(LocalDate.now()
+      .atStartOfDay(ZoneId.systemDefault())
+      .toInstant());
+    JsonParser parser = Mockito.mock(JsonParser.class);
+    Mockito.when(parser.getValueAsString())
+      .thenReturn(expectedResult.toInstant()
+        .atZone(ZoneId.systemDefault())
+        .format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+    // When
+    Date result = deserializer.deserialize(parser, null);
+
+    // Then
+    Assertions.assertEquals(expectedResult, result);
+  }
+
+  @Test
+  void givenInvalidDateWhenDeserializeThenIOException() throws IOException {
+    // Given
+    Date date = Date.from(LocalDate.now()
+      .atStartOfDay(ZoneId.systemDefault())
+      .toInstant());
+    JsonParser parser = Mockito.mock(JsonParser.class);
+    Mockito.when(parser.getValueAsString())
+      .thenReturn(date.toInstant()
+        .atZone(ZoneId.systemDefault())
+        .format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+
+    // When
+    Assertions.assertThrows(IOException.class, () -> deserializer.deserialize(parser, null));
+  }
+
+  @Test
+  void givenEmptyStringWhenThenNull() throws IOException {
+    // Given
+    JsonParser parser = Mockito.mock(JsonParser.class);
+    Mockito.when(parser.getValueAsString())
+      .thenReturn("");
+
+    // When
+    Date result = deserializer.deserialize(parser, null);
+
+    // Then
+    Assertions.assertNull(result);
+  }
+
+  @Test
+  void givenNullStringWhenThenNull() throws IOException {
+    // Given
+    JsonParser parser = Mockito.mock(JsonParser.class);
+    Mockito.when(parser.getValueAsString())
+      .thenReturn(null);
+
+    // When
+    Date result = deserializer.deserialize(parser, null);
+
+    // Then
+    Assertions.assertNull(result);
+  }
+}
+
+
